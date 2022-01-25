05d11aeba322be3d95cff675b56fbcae70d3bbb2
jashan <jashandhiman.hulk@gmail.com>
2022-01-25 16:59:44 +0530

    logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import firebase from "firebase/compat/app";
 import { MdLogout } from "react-icons/md";
+import { Navigate } from "react-router";
 
 const Navbar = () => {
+  const [out, setOut] = useState(false);
+  const logOut = () => {
+    firebase.auth().signOut();
+    setOut(true);
+  };
   return (
     <div className="nav-bar" id="nav-bar">
       <span className="left heading">My Notes</span>
       <button
         className="right"
-        onClick={() => window.confirm("Want to Log-Out ?")}
+        onClick={() => (window.confirm("Want to Log-Out ?") ? logOut() : null)}
       >
+        {out ? <Navigate to={"/"} /> : null}
         <MdLogout />
       </button>
     </div>
